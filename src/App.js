e5704c46fe968f8cd18241aa94b30d8c1440851d
Hasan Nakib <hsnnkb.dev@gmail.com>
2022-08-03 17:32:59 +0100

    Iteration 1 - Initialise project
    Add search.svg, Add and modify provided styles, Create search bar and logo for MovieSearch.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import { useEffect } from 'react';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import SearchIcon from './search.svg';
+
+
+const API_KEY = '45b613b9';
+const BASE_URL = `http://www.omdbapi.com/?apikey=`;
+const API_URL = `${BASE_URL}${API_KEY}`;
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies('shrek');
+  }, []);
+  
+  return(
+    <div className='app'>
+      <h1>MovieSearch</h1>
+
+      <div className="search">
+        <input 
+          type="text" 
+          placeholder='Search for a Movie...'
+          onChange={() => { handleSearch }}
+        />
+        <img src={ SearchIcon } alt="Icon of magnifying glass denoting search" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
