419a1627afbf055f35020fa0964f8c5604922fea
Hasan Nakib <hsnnkb.dev@gmail.com>
2022-08-03 18:52:59 +0100

    Iteration 2 - Search functionality and dynamically render all searches
    Modify styling on main.css, render entire list of relevant media with search terms, Give search bar functionality.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 
 import './App.css';
+import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 
 
@@ -11,32 +12,50 @@ const BASE_URL = `http://www.omdbapi.com/?apikey=`;
 const API_URL = `${BASE_URL}${API_KEY}`;
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
 
-  const searchMovies = async (title) => {
+  useEffect(() => {
+    searchMovies('Shrek');
+  }, []);
+
+  const searchMovies = async title => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    setMovies(data.Search);
   };
 
-  useEffect(() => {
-    searchMovies('shrek');
-  }, []);
-  
-  return(
-    <div className='app'>
-      <h1>MovieSearch</h1>
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
 
       <div className="search">
-        <input 
-          type="text" 
-          placeholder='Search for a Movie...'
-          onChange={() => { handleSearch }}
+        <input
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
         />
-        <img src={ SearchIcon } alt="Icon of magnifying glass denoting search" />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
-    </div>
-  )
-}
 
-export default App
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map(movie => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import React from 'react';
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie" key={movie.imdbID}>
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+      <div>
+        <img 
+          src={movie.Poster === 'N/A'? 'https://via.placeholder.com/400' : movie.Poster}
+          alt={movie.Title}
+        />
+      </div>
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
